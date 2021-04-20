130a66b9694c9d2cd5a20eaadf6bb9ad64c8d624
Logan Bonsignore <Loganbon14@gmail.com>
2021-04-20 14:37:10 -0600

    update table query

--- PCS/FY 2021/execute_table_query.py
+++ PCS/FY 2021/execute_table_query.py
@@ -42,29 +42,36 @@ class PCS_Table:
             pos_indicator += 1
         return pos_structures
 
-    def match_code_to_row(self, pos: str or int, section_codes: dict) -> list:
-        # Args:
-            # pos = position number to check
-            # section_codes = a dictionary keys=position values (4-7) and values=the individual position code
-        # This method searches a particular position value to find a match to the 'section codes' code value passed as arg
-        # It returns a list of all available category objects in all pcs rows
-        # Convert pos to string for query
-        if isinstance(pos, int):
-            pos = str(pos)
-        # Used to store path options
-        options = []
+    def find_component_in_tables(self, pcs_component: str) -> dict:
+        # This method is used to identify a 'category', 'code' and 'pos' value for any 'pcs_component' text returned by an index query
+        # This method iterates through each table and calls find_component_in_table() to identify matching pcs components
+        # This method queries all tables until match is found
+        # It's assummed that each pcs component text is always shown in the same 'category' or 'pos' (NOTE: test this claim)
+        # Create generator object of all pcs tables
+        tables = (table for table in PCS_tables["ICD10PCS.tabular"]["pcsTable"])
+        for table in tables:
+            # Run find_pcs_component_in_table() on each table until match found
+            return self.find_component_in_table(pcs_component, table)
+        # If match is not found, return empty dict
+        raise LookupError(f"Could not find a matching text value to '{pcs_component}' in any pcs table.")
+
+    def find_component_in_table(self, pcs_component: str, table: dict) -> dict:
+        # This method is called in find_component_in_tables()
+        # This method is used to identify a 'category', 'code' and 'pos' value for any pcs_component text returned by an index query
+        # This method queries only one individual table
         for row_container in table["pcsRow"]:
-            # Extract number of codes available in this row
-            num_codes_avail = row_container["_codes"]
             for row in row_container["axis"]:
                 # Search for pos code match at pos passed as arg
-                if row["_pos"] == pos:
-                    for code_choice in row["label"]:
-                        # If 'code_choice' matches individual pos 4 code extracted from index query return
-                        if code_choice["_code"] == section_codes[pos]:
-                            # Use options because user may have the choice to use more than one row
-                            options.append(row)
-        return options
+                for code_container in row["label"]:
+                    # Search for matching text to pcs component
+                    if code_container["__text"] == pcs_component:
+                        # Return data to user
+                        return {
+                            "category": row["title"],
+                            "pos": row["_pos"],
+                            "code": code_container["_code"],}
+        # If match is not found, raise error
+        raise LookupError(f"Could not find a matching text value to '{pcs_component}' in pcs table passed as arg.")
 
 # Aggregating tables into one list
 tables = [table for table in PCS_tables["ICD10PCS.tabular"]["pcsTable"]]
@@ -72,31 +79,16 @@ tables = [table for table in PCS_tables["ICD10PCS.tabular"]["pcsTable"]]
 # Begin query
 query_tool = PCS_Table()
 # Manually coded for testing
-code_from_index = "0U2DXY"
+code_from_index = "0UJ"
 
 # Return table object based on codes passed
 # Must have 3 or more codes available
 table = query_tool.get_table_from_string(code_from_index)
 # Returns dict with keys=position values (4-7) and values=the individual position code
 section_codes = query_tool.define_section_codes(code_from_index)
-for code in section_codes.values(): 
-    pos_count = 1
-    # Query table to build final 'code_output' object using 'section_codes'
-    list_of_options = query_tool.match_code_to_row(pos_count, section_codes)
-    if not list_of_options:
-        # Raise error if faulty code combination (code cannot be found)
-        raise ValueError("Expected list_options list.")
-    pos_count += 1
-if len(list_of_options) == 1:
-    print("HIII")
-    # Auto-select
-    # Add only element of list_of_options list to final 'code object'
-    pass
-else:
-    print("YOOOO")
-    # Ask user what they want
-    pass
-pprint(list_of_options)
+
+print(query_tool.find_component_in_table("Vagina and Cul-de-sac", table))
+
 
 
     
